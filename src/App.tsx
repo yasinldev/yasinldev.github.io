8b4820d06480ddb57482d813a04c0d0cf0be2031
yasinldev <ozkayayasin964@gmail.com>
2025-02-09 16:29:30 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from "./pages/home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={'container'}>
+          <div className={'header'}>
+              <div className={'navbar'}>
+                  <a href={'/'}>Home</a>
+                  <a href={'/'}>About</a>
+                  <a href={'/'}>Articles</a>
+                  <a href={'/'}>Projects</a>
+              </div>
+          </div>
+          <Router>
+          <Routes>
+              <Route path={'/'} element={<Home />} />
+            </Routes>
+          </Router>
+      </div>
   );
 }
 
